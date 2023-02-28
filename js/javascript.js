9a435ce1bdae1c7ed2134db71f4fc699b39ef185
Alexis <alee.otto88@gmail.com>
2023-02-28 20:11:34 -0300

    a

--- js/javascript.js
+++ js/javascript.js
@@ -1,12 +1,12 @@
+//estas son las dos funciones para cambiar la imagen de perfil al pasar el mouse
 function cambiarimg(imagen) {
    document.getElementById(imagen).src = 'https://media.tenor.com/1DgKXzT0hgcAAAAC/michael-scott.gif';
-   
 }
 
 function imagendefault(imagen) {
     document.getElementById(imagen).src = 'https://upload.wikimedia.org/wikipedia/en/d/dc/MichaelScott.png';
 }
-
+//estas son las dos funciones que cambian el tamaño de los iconos al pasar con el mouse
 function aumentar(icono) {
     document.getElementById(icono).width = 35;
     document.getElementById(icono).height=30;
